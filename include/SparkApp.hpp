afebbc91dcb944e4db615138ecc1dc429514fe9b
PyroSoft <47942890+EfeOsman@users.noreply.github.com>
2020-12-05 21:02:35 +0300

    Update SparkApp.hpp

--- include/SparkApp.hpp
+++ include/SparkApp.hpp
@@ -8,30 +8,30 @@ class SparkApp
 
 private:
 	// Variables
-    StateData stateData;
+	StateData stateData;
 	sf::RenderWindow* window;
 	sf::Event sfEvents;
 	sf::Clock clock;
 
 	std::stack<State*> states;
 
-    std::map<std::string, int> suppKeys;
+	std::map<std::string, int> suppKeys;
 
-    // GFX Settings
-    Json::Value gfxConfig;
-    sf::ContextSettings gfxContext;
-    
-    // Style Settings
-    Json::Value styleConfig;
+	// GFX Settings
+	Json::Value gfxConfig;
+	sf::ContextSettings gfxContext;
+	
+	// Style Settings
+	Json::Value styleConfig;
 
-    // DeltaTime
+	// DeltaTime
 	float dt;
 
 	// Initialization
 	void initWindow();
 	void initKeys();
-    void initStateData();
-    void initColorStyles();
+	void initStateData();
+	void initColorStyles();
 	void initStates();
 
 
@@ -39,24 +39,24 @@ private:
 public:
 	// Constructors/Destructors
 
-    SparkApp();
-    ~SparkApp();
+	SparkApp();
+	~SparkApp();
 
-    
+	
 
-    // Main loop functions
-    /* ------------------- */
+	// Main loop functions
+	/* ------------------- */
 
-    // Update
-    void updateDeltaTime();
-    void updateSFMLEvents();
-    void update();
+	// Update
+	void updateDeltaTime();
+	void updateSFMLEvents();
+	void update();
 
-    // Render
-    void render();
+	// Render
+	void render();
 
-    // Core
-    void run();
+	// Core
+	void run();
 
 };
 #endif // SPARK_APP_HPP
